6f0ec0b37d5e45476a1259fe26840dcf30389ed1
Khayal14 <abdelrahmankhayal14@gmail.com>
2026-03-05 15:42:17 +0200

    Update SSL settings in settings.py

--- backend/apex_erp/settings.py
+++ backend/apex_erp/settings.py
@@ -225,7 +225,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Security
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
